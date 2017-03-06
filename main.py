6587dae3e4ace877be16ab1f474aa6e0f37f04c5
John Wu <john_t_wu@trend.com.tw>
2017-03-06 20:25:50 +0800

    Load ini config file

--- main.py
+++ main.py
@@ -5,33 +5,11 @@ import re
 # FCB exchange rate page
 res = requests.get("https://ibank.firstbank.com.tw/NetBank/7/0201.html?sh=none");
 
-#print(res.text)
-
-
-#find spot and cash rate
-target_flag = False
-exchange_rate_set = []
-for row in res.iter_lines():
-    if "U.S. Dollar(USD)" in row:
-        target_flag = True
-    if "Japanese Yen (JPY)" in row:
-        target_flag = True
-    if target_flag:
-        row = row.strip()
-        if re.findall(r"\d+\.\d+",row):
-            #print row
-            exchange_rate_set.append(float(row))
-            target_flag = False
-#print(exchange_rate_set)
-
-for idx, rate in enumerate(exchange_rate_set):
-    if idx%2 == 0:
-        print rate
-        # if rate < USD_target, send email notification
-
 Config = ConfigParser.ConfigParser()
 Config.read("fcber.ini")
-print (Config.sections())
+
+
+
 
 def ConfigSectionMap(section):
     dict1 = {}
@@ -45,3 +23,40 @@ def ConfigSectionMap(section):
             print("exception on %s!" % option)
             dict1[option] = none
     return dict1
+
+def main():
+    #find spot and cash rate
+    target_flag = False
+    exchange_rate_set = []
+    for row in res.iter_lines():
+        if "U.S. Dollar(USD)" in row:
+            target_flag = True
+        if "Japanese Yen (JPY)" in row:
+            target_flag = True
+        if target_flag:
+            row = row.strip()
+            if re.findall(r"\d+\.\d+",row):
+                #print row
+                exchange_rate_set.append(float(row))
+                target_flag = False
+    #print(exchange_rate_set)
+    
+    for idx, rate in enumerate(exchange_rate_set):
+        if idx%2 == 0:
+            print rate
+            # if rate < USD_target, send email notification
+    
+    
+    #print (Config.sections())
+    MAX_USD = ConfigSectionMap("Expect Rule")['max_usd']
+    MIN_USD = ConfigSectionMap("Expect Rule")['min_usd']
+    MAX_JPY = ConfigSectionMap("Expect Rule")['max_jpy']
+    MIN_JPY = ConfigSectionMap("Expect Rule")['min_jpy']
+    print (MAX_USD, MIN_USD, MAX_JPY, MIN_JPY)
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
